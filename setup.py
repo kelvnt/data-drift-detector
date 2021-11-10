486e50acd129a8c62c8e05efe7af865253df58c8
kelvnt <btkelvin@gmail.com>
2021-11-10 23:50:00 +0800

    switched to GPL v3 license

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    license="MIT license",
+    license="GPLv3",
     install_requires=[
         "category-encoders>=2.2.2",
         "matplotlib>=3.4.3",
