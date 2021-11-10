ac654a1e940d215ebb7a72311313b78a091794e5
kelvnt <btkelvin@gmail.com>
2021-11-10 23:43:10 +0800

    minor bugfixes, re-run example notebook

--- data_drift_detector/data_drift_detector.py
+++ data_drift_detector/data_drift_detector.py
@@ -70,8 +70,8 @@ class DataDriftDetector:
         assert isinstance(numeric_columns, (list, type(None))),\
             "numeric_columns should be of type list"
 
-        df_prior_ = copy.deepcopy(df_prior)
-        df_post_ = copy.deepcopy(df_post)
+        df_prior_ = df_prior.copy()
+        df_post_ = df_post.copy()
 
         if categorical_columns is None:
             categorical_columns = (
@@ -388,7 +388,7 @@ class DataDriftDetector:
         
         for i, col in enumerate(plot_categorical_columns):
             if len(plot_categorical_columns) == 1:
-                _ax = ax[0]
+                _ax = ax
             elif len(plot_categorical_columns) > 1:
                 _ax = ax[i]
 
@@ -545,8 +545,8 @@ class DataDriftDetector:
         split (if necessary).
         """
 
-        df_post = copy.deepcopy(self.df_post)
-        train_prior = copy.deepcopy(self.df_prior)
+        df_post = self.df_post.copy()
+        train_prior = self.df_prior.copy()
         
         # drop NAs
         cols = self.categorical_columns+self.numeric_columns
@@ -565,11 +565,11 @@ class DataDriftDetector:
             df_post = shuffle(df_post)
             n_split = int(len(df_post)*self.train_size)
 
-            train_post = df_post.iloc[:n_split]
-            test = df_post.iloc[n_split:]
+            train_post = df_post.iloc[:n_split].copy()
+            test = df_post.iloc[n_split:].copy()
 
         else:
-            test = copy.deepcopy(self.test_data)
+            test = self.test_data.copy()
             test = test[cols].dropna(how='any')
             train_post = df_post
 
@@ -597,8 +597,8 @@ class DataDriftDetector:
         train_post = df[df.source == 'Train Post'].drop('source', axis=1)
 
         # CatBoostEncoder for high cardinality columns
-        test_prior = copy.deepcopy(test)
-        test_post = copy.deepcopy(test)
+        test_prior = test.copy()
+        test_post = test.copy()
 
         tf_prior = CatBoostEncoder(cols=high_cardinality_columns,
                                    random_state=self.random_state)
